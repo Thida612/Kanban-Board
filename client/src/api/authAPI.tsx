950265a9e13622434cd16c5d76afda2afb7f9897
Thida612 <angelic722@gmail.com>
2025-01-28 21:33:04 -0600

    added code of send a post request to the '/auth.login' endpoint with user infomation

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -2,4 +2,14 @@ import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
   // TODO: make a POST request to the login route
-  
+  try {
+     // Sending a POST request to the '/auth/login' endpoint with user information
+    const response = await fetch('/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userInfo),
+    });
+
+    
