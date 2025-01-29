8ea72931669e1c4864aa71ff2df12aa9bb3e3a3f
Thida612 <angelic722@gmail.com>
2025-01-28 22:13:18 -0600

    add code to return the token

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -3,20 +3,39 @@ import { JwtPayload, jwtDecode } from 'jwt-decode';
 class AuthService {
   getProfile() {
     // TODO: return the decoded token
-    // Retrieve the token from storage
-    const token = this.getToken();
-    // If a token exists, decode it to get the payload, otherwise return null
-    return token ? jwtDecode<JwtPayload>(token) : null;
-// Function to check if the user is logged in
-loggedIn(){
-
-  // TODO: return a value that indicates if the user is logged in
-  const token = this.getToken();
-  // Check if the token exists and is not expired
-  return !!token && !this.isTokenExpired(token);
-}
-
-isTokenExpired(token: string) {
-
-
+     // TODO: return the decoded token
+     const token = this.getToken();
+     return token ? jwtDecode<JwtPayload>(token) : null;
+   }
+ 
+   loggedIn() {
+     // TODO: return a value that indicates if the user is logged in
+     const token = this.getToken();
+     return !!token && !this.isTokenExpired(token);
+   }
    
+   isTokenExpired(token: string) {
+
+        // TODO: return a value that indicates if the token is expired
+        try {
+          const decoded = jwtDecode<JwtPayload>(token);
+          if (decoded.exp) {
+            const expirationTime = decoded.exp * 1000;
+            return Date.now() > expirationTime;
+          }
+          return false;
+        } catch (error) {
+          return true;
+        }
+      }
+    
+      getToken(): string | null {
+
+            // TODO: return the token
+    return localStorage.getItem('id_token');
+  }
+
+  login(idToken: string) {
+
+    
+ 
