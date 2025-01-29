59359d379c08e2b383be37e464b30eb97febcb26
Thida612 <angelic722@gmail.com>
2025-01-28 22:36:34 -0600

    Add authentication to the API routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -7,4 +7,8 @@ const router = Router();
 
 router.use('/auth', authRoutes);
 // TODO: Add authentication to the API routes
+router.use('/api', authenticateToken, apiRoutes);
+
+export default router;
+
 
