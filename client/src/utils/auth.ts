34f2e0e8b65d4bcc9a1a818cf1f360825bfc0020
Thida612 <angelic722@gmail.com>
2025-01-28 22:07:50 -0600

    add code to return a value that indicates if the user is logged in and code to check if the token exists and is not expired

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -3,14 +3,20 @@ import { JwtPayload, jwtDecode } from 'jwt-decode';
 class AuthService {
   getProfile() {
     // TODO: return the decoded token
-
     // Retrieve the token from storage
     const token = this.getToken();
-
     // If a token exists, decode it to get the payload, otherwise return null
     return token ? jwtDecode<JwtPayload>(token) : null;
-  }
 // Function to check if the user is logged in
-loggedIn() {
+loggedIn(){
+
+  // TODO: return a value that indicates if the user is logged in
+  const token = this.getToken();
+  // Check if the token exists and is not expired
+  return !!token && !this.isTokenExpired(token);
+}
+
+isTokenExpired(token: string) {
+
 
    
