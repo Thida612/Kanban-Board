3fa6ce77c123ce31eec4d286cd1e371475394cab
Thida612 <angelic722@gmail.com>
2025-01-28 22:04:00 -0600

    add code to retrieve token from storage, and function to check if the user is logged in

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -3,4 +3,14 @@ import { JwtPayload, jwtDecode } from 'jwt-decode';
 class AuthService {
   getProfile() {
     // TODO: return the decoded token
+
+    // Retrieve the token from storage
+    const token = this.getToken();
+
+    // If a token exists, decode it to get the payload, otherwise return null
+    return token ? jwtDecode<JwtPayload>(token) : null;
+  }
+// Function to check if the user is logged in
+loggedIn() {
+
    
