6b009e71949ef7b341f620db8f6bfe881c7b3a88
Thida612 <angelic722@gmail.com>
2025-01-28 21:36:47 -0600

    add code for response that is not OK, Parsing the response data as Json

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -12,4 +12,25 @@ const login = async (userInfo: UserLogin) => {
       body: JSON.stringify(userInfo),
     });
 
+     // Checking if the response is not OK (status code is not in the range 200-299)
+
+    if (!response.ok) {
+      throw new Error('Failed to login');
+    }
+
+     // Parsing the response data as JSON
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error during login:', error);
+    throw error;
+  }
+};
+
+
+export { login };
+
+
+
     
